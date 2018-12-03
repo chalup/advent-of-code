9cceffd00199e82e28aed4abfcdea8db5e341f9f
Jerzy Chalupski <jerzy.chalupski@ig.com>
2018-12-03 07:15:16 +0100

    Add claim id to our data structures

--- src/main/java/org/chalup/advent2018/Day3.kt
+++ src/main/java/org/chalup/advent2018/Day3.kt
@@ -1,20 +1,20 @@
 package org.chalup.advent2018
 
 object Day3 {
-    data class Claim(val x: Int, val y: Int, val width: Int, val height: Int)
-    data class Coords(val x: Int, val y: Int)
+    data class Claim(val id: Int, val x: Int, val y: Int, val width: Int, val height: Int)
+    data class ClaimedCoords(val claimId: Int, val x: Int, val y: Int)
 
-    private val claimRegex = """^#\d+ @ (\d+),(\d+): (\d+)x(\d+)$""".toRegex()
+    private val claimRegex = """^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$""".toRegex()
 
     private fun parseClaim(claim: String): Claim = claimRegex
         .matchEntire(claim)!!.groupValues.drop(1)
         .map { it.toInt() }
-        .let { (x, y, w, h) -> Claim(x, y, w, h) }
+        .let { (id, x, y, w, h) -> Claim(id, x, y, w, h) }
 
-    private fun Claim.allClaimedCoords(): Set<Coords> = mutableSetOf<Coords>().apply {
+    private fun Claim.allClaimedCoords(): Set<ClaimedCoords> = mutableSetOf<ClaimedCoords>().apply {
         for (px in x until x + width) {
             for (py in y until y + height) {
-                add(Coords(px, py))
+                add(ClaimedCoords(id, px, py))
             }
         }
     }
@@ -23,7 +23,7 @@ object Day3 {
         claims
             .map { parseClaim(it) }
             .flatMap { it.allClaimedCoords() }
-            .groupingBy { it }
+            .groupingBy { it.x to it.y }
             .eachCount()
             .count { (_, claims) -> claims > 1 }
 }
