d716d5ccee4b67ba779cb32a16a1023051c238e3
Jerzy Chalupski <jerzy.chalupski@ig.com>
2019-12-08 11:36:52 +0100

    2019 - Day 7, part 2 (turbo ugly version)

--- src/main/java/org/chalup/advent2019/Day7.kt
+++ src/main/java/org/chalup/advent2019/Day7.kt
@@ -4,6 +4,8 @@ import org.chalup.advent2019.IntcodeInterpreter.ProgramResult.ExecutionError
 import org.chalup.advent2019.IntcodeInterpreter.ProgramResult.Finished
 import java.util.concurrent.BlockingQueue
 import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.TimeUnit.MILLISECONDS
+import kotlin.concurrent.thread
 
 object Day7 {
     fun calculateMaxThrusterInput(programInput: String): Int {
@@ -15,6 +17,53 @@ object Day7 {
             .map { config -> calculateThrust(program, config) }
             .max()!!
     }
+
+    fun calculateMaxThrusterInputWithFeedbackLoop(programInput: String): Int {
+        val program = programInput.split(",").map(String::toInt)
+
+        val phaseConfigs = permutations((5..9).toSet())
+
+        return phaseConfigs
+            .map { phaseConfig ->
+                val programs = pipeline(program, phaseConfig)
+                val input = programs.first().input
+                val output = programs.last().output
+                var result = 0
+
+                input.put(0)
+                while (programs.any { it.isRunning() }) {
+                    val pipelineOutput = output.poll(1, MILLISECONDS)
+                    if (pipelineOutput != null) {
+                        input.put(pipelineOutput)
+                        result = pipelineOutput
+                    }
+                }
+
+                result
+            }
+            .max()!!
+    }
+
+    private fun pipeline(program: List<Int>, phases: List<Int>): List<IntcodeProgram> {
+        val inputs = phases.map { phase -> LinkedBlockingQueue<Int>(listOf(phase)) }
+        val outputs = inputs.drop(1).toMutableList().apply { add(LinkedBlockingQueue()) }
+
+        return inputs.zip(outputs) { input, output -> IntcodeProgram(program, input, output) }
+    }
+
+    private class IntcodeProgram(val program: List<Int>,
+                                 val input: BlockingQueue<Int>,
+                                 val output: BlockingQueue<Int>) {
+        private val thread = thread {
+            when (val result = IntcodeInterpreter.execute(program, input, output)) {
+                is ExecutionError -> throw RuntimeException(result.getErrorMessage())
+                is Finished -> Unit
+            }
+        }
+
+        fun isRunning() = thread.isAlive
+    }
+
 
     private fun calculateThrust(program: List<Int>, phaseConfig: List<Int>): Int {
         return phaseConfig.fold(0) { signal, phase ->
