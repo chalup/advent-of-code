9633356e7976afc1c8193118c3c1b303baf5c597
Jerzy Chalupski <jerzy@getbase.com>
2020-12-09 09:32:44 +0100

    2020 Day 1 proper solution

--- src/main/java/org/chalup/advent2020/Day1.kt
+++ src/main/java/org/chalup/advent2020/Day1.kt
@@ -3,23 +3,15 @@ package org.chalup.advent2020
 import com.google.common.collect.Sets
 
 object Day1 {
-    fun task1(input: List<String>): Int {
-        val numbers = input.mapTo(mutableSetOf()) { it.toInt() }
+    fun task1(input: List<String>): Int = solve(input, 2)
+    fun task2(input: List<String>): Int = solve(input, 3)
 
-        return numbers
-            .filter { (2020 - it) in numbers }
-            .fold(1, Int::times)
-    }
-
-    fun task2(input: List<String>): Int {
-        val numbers = input.mapTo(mutableSetOf()) { it.toInt() }
-
-        return numbers
-            .asSequence()
-            .flatMap { a -> numbers.asSequence().filter { it != a }.filter { it + a < 2020 }.map { a to it } }
-            .flatMap { (a, b) -> numbers.asSequence().filter { it != a && it != b }.map { Triple(a, b, it) } }
-            .filter { (a, b, c) -> a + b + c == 2020 }
-            .map { (a, b, c) -> a * b * c }
-            .first()
-    }
+    @Suppress("UnstableApiUsage")
+    private fun solve(input: List<String>, n: Int): Int = input
+        .mapTo(mutableSetOf(), String::toInt)
+        .let { Sets.combinations(it, n) }
+        .asSequence()
+        .filter { it.reduce(Int::plus) == 2020 }
+        .map { it.reduce(Int::times) }
+        .first()
 }
