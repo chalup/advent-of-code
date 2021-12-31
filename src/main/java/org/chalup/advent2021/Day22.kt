fc8cc5f07239fbe36660745f18ff27f3eb51afa9
Jerzy Chalupski <jerzy@getbase.com>
2021-12-31 13:24:59 +0100

    2021 Day 22 part 2

--- src/main/java/org/chalup/advent2021/Day22.kt
+++ src/main/java/org/chalup/advent2021/Day22.kt
@@ -4,19 +4,18 @@ import org.chalup.utils.Vector3
 import org.chalup.utils.match
 
 object Day22 {
-    fun task1(input: List<String>): Int = input
+    fun task1(input: List<String>): Long = input
         .map(this::parseInstruction)
         .filter { it.isInitializationInstruction() }
-        .fold(setOf<Vector3>()) { enabledPoints, (enable, cube) ->
-            if (enable) {
-                enabledPoints + cube.points()
-            } else {
-                enabledPoints - cube.points()
-            }
-        }
-        .size
+        .fold(emptySet<Cube>()) { cubes, instruction -> cubes.plus(instruction) }
+        .fold(0L) { count, cube -> count + with(cube) { xs.size() * ys.size() * zs.size() } }
 
-    private data class Cube(
+    fun task2(input: List<String>): Long = input
+        .map(this::parseInstruction)
+        .fold(emptySet<Cube>()) { cubes, instruction -> cubes.plus(instruction) }
+        .fold(0L) { count, cube -> count + with(cube) { xs.size() * ys.size() * zs.size() } }
+
+    internal data class Cube(
         val xs: LongRange,
         val ys: LongRange,
         val zs: LongRange,
@@ -34,6 +33,77 @@ object Day22 {
         val cube: Cube,
     )
 
+    // the set consists of disjointed cubes
+    private fun Set<Cube>.plus(instruction: Instruction): Set<Cube> = buildSet {
+        for (cube in this@plus) {
+            when {
+                // The cube from instruction is already accounted for; the set consists of disjointed cubes,
+                // so it won't change, and we can return immediately.
+                instruction.cube in cube && instruction.enable -> return this@plus
+                // The cube will be either overwritten by a larger cube (if enable == true) or removed (if enabled == false)
+                cube in instruction.cube -> continue
+                cube intersects instruction.cube -> addAll(cube - instruction.cube)
+                else -> add(cube)
+            }
+        }
+
+        if (instruction.enable) add(instruction.cube)
+    }
+
+    internal infix operator fun Cube.contains(other: Cube): Boolean = when {
+        other.xs !in this.xs -> false
+        other.ys !in this.ys -> false
+        other.zs !in this.zs -> false
+        else -> true
+    }
+
+    internal infix operator fun LongRange.contains(other: LongRange): Boolean =
+        other.first >= this.first && other.last <= this.last
+
+    internal infix fun Cube.intersects(other: Cube): Boolean =
+        other.xs intersects this.xs && other.ys intersects this.ys && other.zs intersects this.zs
+
+    internal infix fun LongRange.intersects(other: LongRange): Boolean =
+        this.first in other || this.last in other || other.first in this || other.last in this
+
+    internal operator fun Cube.minus(other: Cube): Set<Cube> = buildSet {
+        val xRanges = (xs sliceWith other.xs)
+        val yRanges = (ys sliceWith other.ys)
+        val zRanges = (zs sliceWith other.zs)
+
+        for (xRange in xRanges) {
+            for (yRange in yRanges) {
+                for (zRange in zRanges) {
+                    val cube = Cube(xRange, yRange, zRange)
+
+                    if (!(cube intersects other)) {
+                        add(cube)
+                    }
+                }
+            }
+        }
+    }
+
+    internal fun LongRange.size(): Long = (last - first + 1).takeUnless { isEmpty() } ?: 0L
+
+    private infix fun LongRange.sliceWith(other: LongRange): Set<LongRange> = when {
+        this in other -> sequenceOf(this)
+        other in this -> sequenceOf(
+            this.first until other.first,
+            other,
+            (other.last + 1)..this.last,
+        )
+        other.first in this -> sequenceOf(
+            this.first until other.first,
+            other.first..this.last
+        )
+        other.last in this -> sequenceOf(
+            this.first..other.last,
+            (other.last + 1)..this.last
+        )
+        else -> sequenceOf()
+    }.filterNotTo(mutableSetOf()) { it.isEmpty() }
+
     private fun parseInstruction(line: String): Instruction = match(line) {
         pattern("""(on|off) x=$RANGE,y=$RANGE,z=$RANGE""") { (onOff, x1, x2, y1, y2, z1, z2) ->
             Instruction(
@@ -47,15 +117,11 @@ object Day22 {
         }
     }
 
-    private fun Instruction.isInitializationInstruction() = cube.xs.isInInitializationRange() &&
-            cube.ys.isInInitializationRange() &&
-            cube.xs.isInInitializationRange()
+    private fun Instruction.isInitializationInstruction() = cube.xs in INITIALIZATION_RANGE &&
+            cube.ys in INITIALIZATION_RANGE &&
+            cube.zs in INITIALIZATION_RANGE
 
-    private fun LongRange.isInInitializationRange() = when {
-        first < -50 -> false
-        last > 50 -> false
-        else -> true
-    }
+    private val INITIALIZATION_RANGE = -50L..50L
 
     private const val DIGIT = """([-\d]+)"""
     private const val RANGE = """$DIGIT..$DIGIT"""
---
+++ src/test/java/org/chalup/advent2021/Day22Test.kt
@@ -0,0 +1,220 @@
+package org.chalup.advent2021
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import org.chalup.advent2021.Day22.Cube
+import org.chalup.advent2021.Day22.contains
+import org.chalup.advent2021.Day22.intersects
+import org.chalup.advent2021.Day22.minus
+import org.chalup.advent2021.Day22.size
+import org.chalup.utils.Vector3
+
+class Day22Test : FreeSpec({
+    "Ranges size" - {
+        val testCases = listOf(
+            1L..2L to 2L,
+            1L..1L to 1L,
+            LongRange.EMPTY to 0L
+        )
+
+        testCases.forEach { (input, expected) ->
+            "[$input].size should be $expected" {
+                input.size() shouldBe expected
+            }
+        }
+    }
+
+    "Ranges intersections" - {
+        val testCases = listOf(
+            Triple(5L..10L, 12L..13L, false),
+            Triple(6L..10L, 0L..5L, false),
+            Triple(5L..10L, 8L..12L, true),
+            Triple(5L..10L, 1L..6L, true),
+            Triple(5L..10L, 6L..8L, true),
+            Triple(5L..10L, 1L..12L, true),
+            Triple(5L..10L, 10L..12L, true),
+            Triple(5L..10L, 1L..5L, true),
+        )
+
+        testCases.forEach { (rangeA, rangeB, expected) ->
+            "$rangeA ${if (expected) "intersects" else "does not intersect"} $rangeB" {
+                (rangeA intersects rangeB) shouldBe expected
+            }
+        }
+    }
+
+    "Ranges contains" - {
+        val testCases = listOf(
+            Triple(5L..10L, 12L..13L, false),
+            Triple(6L..10L, 0L..5L, false),
+            Triple(5L..10L, 8L..12L, false),
+            Triple(5L..10L, 1L..6L, false),
+            Triple(5L..10L, 6L..8L, true),
+            Triple(6L..8L, 5L..10L, false),
+            Triple(5L..10L, 1L..12L, false),
+            Triple(1L..12L, 5L..10L, true),
+        )
+
+        testCases.forEach { (rangeA, rangeB, expected) ->
+            "$rangeA ${if (expected) "contains" else "does not contain"} $rangeB" {
+                (rangeA contains rangeB) shouldBe expected
+            }
+        }
+    }
+
+    "Cube contains" - {
+        val testCases = listOf(
+            Triple(
+                Cube(1L..2L, 1L..2L, 1L..2L),
+                Cube(3L..4L, 3L..4L, 3L..4L),
+                false
+            ),
+            Triple(
+                Cube(1L..2L, 1L..2L, 1L..2L),
+                Cube(1L..1L, 1L..1L, 1L..1L),
+                true
+            ),
+            Triple(
+                Cube(1L..1L, 1L..1L, 1L..1L),
+                Cube(1L..2L, 1L..2L, 1L..2L),
+                false
+            ),
+            Triple(
+                Cube(1L..2L, 1L..2L, 1L..2L),
+                Cube(2L..3L, 2L..3L, 2L..3L),
+                false
+            ),
+        )
+
+        testCases.forEach { (cubeA, cubeB, expected) ->
+            "$cubeA ${if (expected) "contains" else "does not contain"} $cubeB" {
+                (cubeA contains cubeB) shouldBe expected
+            }
+        }
+    }
+
+    "Cube intersects" - {
+        val testCases = listOf(
+            Triple(
+                Cube(1L..2L, 1L..2L, 1L..2L),
+                Cube(3L..4L, 3L..4L, 3L..4L),
+                false
+            ),
+            Triple(
+                Cube(1L..2L, 1L..2L, 1L..2L),
+                Cube(3L..4L, 1L..2L, 1L..2L),
+                false
+            ),
+            Triple(
+                Cube(1L..2L, 1L..2L, 1L..2L),
+                Cube(1L..1L, 1L..1L, 1L..1L),
+                true
+            ),
+            Triple(
+                Cube(1L..1L, 1L..1L, 1L..1L),
+                Cube(1L..2L, 1L..2L, 1L..2L),
+                true
+            ),
+            Triple(
+                Cube(1L..2L, 1L..2L, 1L..2L),
+                Cube(2L..3L, 2L..3L, 2L..3L),
+                true
+            ),
+        )
+
+        testCases.forEach { (cubeA, cubeB, expected) ->
+            "$cubeA ${if (expected) "intersects" else "does not intersect"} $cubeB" {
+                (cubeA intersects cubeB) shouldBe expected
+            }
+        }
+    }
+
+    "Cube difference" - {
+        val testCases = listOf(
+            Triple(
+                Cube(1L..2L, 1L..2L, 1L..2L),
+                Cube(1L..1L, 1L..1L, 1L..1L),
+                setOf(
+                    Cube(1L..2L, 1L..2L, 2L..2L),
+                    Cube(2L..2L, 1L..2L, 1L..1L),
+                    Cube(1L..1L, 2L..2L, 1L..1L),
+                )
+            ),
+            Triple(
+                Cube(1L..2L, 1L..2L, 1L..2L),
+                Cube(0L..1L, 0L..1L, 0L..1L),
+                setOf(
+                    Cube(1L..2L, 1L..2L, 2L..2L),
+                    Cube(2L..2L, 1L..2L, 1L..1L),
+                    Cube(1L..1L, 2L..2L, 1L..1L),
+                )
+            ),
+            Triple(
+                Cube(1L..2L, 1L..2L, 1L..2L),
+                Cube(2L..2L, 0L..3L, 0L..3L),
+                setOf(
+                    Cube(1L..1L, 1L..2L, 1L..2L),
+                )
+            ),
+            Triple(
+                Cube(0L..2L, 0L..2L, 0L..2L),
+                Cube(1L..1L, 2L..3L, 1L..1L),
+                setOf(
+                    Cube(0L..2L, 0L..1L, 0L..2L),
+                    Cube(0L..2L, 2L..2L, 0L..0L),
+                    Cube(0L..2L, 2L..2L, 2L..2L),
+                    Cube(0L..0L, 2L..2L, 1L..1L),
+                    Cube(2L..2L, 2L..2L, 1L..1L),
+                )
+            ),
+            Triple(
+                Cube(0L..2L, 0L..2L, 0L..2L),
+                Cube(1L..1L, -1L..3L, 1L..1L),
+                setOf(
+                    Cube(0L..2L, 0L..2L, 0L..0L),
+                    Cube(0L..2L, 0L..2L, 2L..2L),
+                    Cube(0L..0L, 0L..2L, 1L..1L),
+                    Cube(2L..2L, 0L..2L, 1L..1L),
+                )
+            ),
+            Triple(
+                Cube(0L..2L, 0L..2L, 0L..2L),
+                Cube(-1L..3L, -1L..3L, 1L..1L),
+                setOf(
+                    Cube(0L..2L, 0L..2L, 0L..0L),
+                    Cube(0L..2L, 0L..2L, 2L..2L),
+                )
+            ),
+            Triple(
+                Cube(0L..2L, 0L..2L, 0L..2L),
+                Cube(1L..3L, -1L..3L, 1L..3L),
+                setOf(
+                    Cube(0L..2L, 0L..2L, 0L..0L),
+                    Cube(0L..0L, 0L..2L, 1L..2L),
+                )
+            ),
+        )
+
+        testCases.forEach { (cubeA, cubeB, equivalentCubeSet) ->
+            "$cubeA - $cubeB" {
+                // we should have the expected voxels
+                (cubeA - cubeB).points() shouldBe equivalentCubeSet.points()
+
+                // the set should be a disjoint set
+                (cubeA - cubeB).run {
+                    val pairs = this.flatMap { cube ->
+                        this.mapNotNull { otherCube ->
+                            (cube to otherCube).takeUnless { cube === otherCube }
+                        }
+                    }
+
+                    pairs.forEach { (a, b) ->
+                        a intersects b shouldBe false
+                    }
+                }
+            }
+        }
+    }
+})
+
+private fun Set<Cube>.points() = fold(emptySet<Vector3>()) { points, cube -> points + cube.points() }
