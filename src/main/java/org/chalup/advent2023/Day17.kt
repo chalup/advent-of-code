a30ccc66116bd24bd6718edd6db2abd44183b129
Jerzy Chalupski <jerzy@chess.com>
2023-12-17 11:00:21 +0100

    2023 Day 17 part 2

--- src/main/java/org/chalup/advent2023/Day17.kt
+++ src/main/java/org/chalup/advent2023/Day17.kt
@@ -10,7 +10,23 @@ import org.chalup.utils.plus
 import java.util.PriorityQueue
 
 object Day17 {
-    fun task1(input: List<String>): Int {
+    fun task1(input: List<String>): Int = findPath(
+        input,
+        canMoveForward = { it < 3 },
+        canTurn = { true }
+    )
+
+    fun task2(input: List<String>): Int = findPath(
+        input,
+        canMoveForward = { it < 10 },
+        canTurn = { it >= 4 }
+    )
+
+    private fun findPath(
+        input: List<String>,
+        canMoveForward: (Int) -> Boolean,
+        canTurn: (Int) -> Boolean
+    ): Int {
         val bounds = Rect(0, 0, input.first().lastIndex, input.lastIndex)
         val seen = mutableSetOf<Head>()
         val heads = PriorityQueue<Pair<Head, Int>>(compareBy { (_, heatLoss) -> heatLoss }).apply {
@@ -18,7 +34,7 @@ object Day17 {
                 Head(
                     Point(0, 0),
                     direction = it,
-                    stepsBeforeTurn = 3
+                    stepsForward = 0
                 ) to 0
             }
         }
@@ -32,40 +48,41 @@ object Day17 {
                 .takeIf { it in bounds }
                 ?: continue
             val updatedHeatLoss = heatLoss + input[newLocation].digitToInt()
+            val stepsForward = head.stepsForward + 1
 
             if (newLocation == bounds.bottomRight) return updatedHeatLoss
 
-            if (head.stepsBeforeTurn > 1) {
+            if (canMoveForward(stepsForward)) {
                 heads.add(
                     Head(
                         newLocation,
                         head.direction,
-                        stepsBeforeTurn = head.stepsBeforeTurn - 1
+                        stepsForward = stepsForward
                     ) to updatedHeatLoss
                 )
             }
 
-            for (d in sequenceOf(head.direction.cycleNext(), head.direction.cyclePrev())) {
-                heads.add(
-                    Head(
-                        newLocation,
-                        d,
-                        stepsBeforeTurn = 3
-                    ) to updatedHeatLoss
-                )
+            if (canTurn(stepsForward)) {
+                for (d in sequenceOf(head.direction.cycleNext(), head.direction.cyclePrev())) {
+                    heads.add(
+                        Head(
+                            newLocation,
+                            d,
+                            stepsForward = 0
+                        ) to updatedHeatLoss
+                    )
+                }
             }
         }
 
         throw IllegalStateException("Couldn't find the route :(")
     }
 
-    fun task2(input: List<String>): Int = TODO()
-
     private operator fun List<String>.get(point: Point) = this[point.y][point.x]
 
     private data class Head(
         val location: Point,
         val direction: Direction,
-        val stepsBeforeTurn: Int,
+        val stepsForward: Int,
     )
 }
