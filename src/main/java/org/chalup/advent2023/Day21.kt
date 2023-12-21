22f7ce9d4a78d4a78cd63cca71e875848accbe0d
Jerzy Chalupski <jerzy@chess.com>
2023-12-21 18:41:57 +0100

    2023 Day 21, part 2

--- src/main/java/org/chalup/advent2023/Day21.kt
+++ src/main/java/org/chalup/advent2023/Day21.kt
@@ -8,11 +8,12 @@ import org.chalup.utils.manhattanDistance
 import org.chalup.utils.plus
 import org.chalup.utils.points
 import java.util.LinkedList
+import java.util.PriorityQueue
 
 object Day21 {
-    fun task1(map: List<String>): Int {
-        operator fun List<String>.get(p: Point) = this[p.y][p.x]
+    private operator fun List<String>.get(p: Point) = this[p.y][p.x]
 
+    fun task1(map: List<String>): Int {
         val bounds = Rect(0, 0, map.first().lastIndex, map.lastIndex)
         val elf = bounds.points().single { map[it] == 'S' }
 
@@ -35,5 +36,56 @@ object Day21 {
         }
 
         return seen.count { manhattanDistance(it, elf) % 2 == 0 }
+    }
+
+    fun task2(map: List<String>): Long {
+        val bounds = Rect(0, 0, map.first().lastIndex, map.lastIndex)
+        val elf = bounds.points().single { map[it] == 'S' }
+
+        val costWithinBounds = buildMap {
+            val queue = PriorityQueue<Pair<Point, Int>>(compareBy { (_, steps) -> steps }).apply { add(elf to 0) }
+            while (queue.isNotEmpty()) {
+                val (position, stepsTaken) = queue.poll()
+
+                if (position in this) continue
+                put(position, stepsTaken)
+
+                Direction.values()
+                    .mapNotNullTo(queue) { direction ->
+                        (position + direction.vector)
+                            .takeIf { it in bounds }
+                            ?.takeUnless { map[it] == '#' }
+                            ?.let { it to (stepsTaken + 1) }
+                    }
+            }
+        }
+
+        // Grid is 131x131 square, the NSEW paths are clear of obstacles,
+        // the elf starts dead center in the middle and 26501365 % 131 is
+        // conveniently 65.
+        //
+        // So the shape we're looking at is the diamond with some corners
+        // cut. The number of steps is odd and the sector size is odd, so
+        // the sectors will alternate between the ones where we're looking
+        // for odd number of steps and even number of steps.
+        //
+        // At the edges of the shape we need to account for the corners, but
+        // they can be arranged in groups of N/N+1 corners. Too lazy to draw
+        // an ASCII art here, but grab the paper and the pencil and see for
+        // yourself.
+
+        val n = (26501365 / 131).toLong()
+
+        val viablePositionsInOddSectors = costWithinBounds.count { (_, steps) -> steps % 2 == 1 }
+        val viablePositionsInEvenSectors = costWithinBounds.count { (_, steps) -> steps % 2 == 0 }
+        val viablePositionsInCornersOfOddSectors = costWithinBounds.count { (_, steps) -> steps > 65 && steps % 2 == 1 }
+        val viablePositionsInCornersOfEvenSectors = costWithinBounds.count { (_, steps) -> steps > 65 && steps % 2 == 0 }
+
+        return listOf(
+            (n + 1) * (n + 1) * viablePositionsInOddSectors,
+            n * n * viablePositionsInEvenSectors,
+            n * viablePositionsInCornersOfEvenSectors,
+            -(n + 1) * viablePositionsInCornersOfOddSectors
+        ).sum()
     }
 }
--- src/main/java/org/chalup/advent2023/Main.kt
+++ src/main/java/org/chalup/advent2023/Main.kt
@@ -52,6 +52,7 @@ fun main(args: Array<String>) {
         advent(day = 20, part = 1, Day20::task1)
         advent(day = 20, part = 2, Day20::task2)
         advent(day = 21, part = 1, Day21::task1)
+        advent(day = 21, part = 2, Day21::task2)
     }
 
     println("=== TOTAL [${elapsedTotal}ms] ===")
