ae8c96d18f732d1fe922f50418efba2bed61570c
Jerzy Chalupski <jerzy@chess.com>
2023-12-23 13:43:46 +0100

    2023 Day 23, part 1

---
+++ src/main/java/org/chalup/advent2023/Day23.kt
@@ -0,0 +1,99 @@
+package org.chalup.advent2023
+
+import org.chalup.utils.Direction
+import org.chalup.utils.Point
+import org.chalup.utils.Rect
+import org.chalup.utils.contains
+import org.chalup.utils.plus
+import java.util.LinkedList
+
+object Day23 {
+    fun task1(map: List<String>): Int {
+        operator fun List<String>.get(p: Point) = this[p.y][p.x]
+        val bounds = Rect(0, 0, map.first().lastIndex, map.lastIndex)
+
+        val startLocation = map.first().indexOf('.').let { Point(it, 0) }
+        val finishLocation = map.last().indexOf('.').let { Point(it, map.lastIndex) }
+
+        val segments = buildSet<PathSegment> {
+            fun traversePathSegment(startingPoint: PathSegmentStart): PathSegment? {
+                val path = mutableSetOf(startingPoint.location)
+
+                var currentLocation = startingPoint.location
+                var nextStep: Direction? = startingPoint.direction
+                while (nextStep != null) {
+                    currentLocation += nextStep.vector
+                    path += currentLocation
+
+                    if (currentLocation == finishLocation) break
+
+                    when (map[currentLocation]) {
+                        '>' -> if (nextStep != Direction.R) return null
+                        '<' -> if (nextStep != Direction.L) return null
+                        'v' -> if (nextStep != Direction.D) return null
+                        '^' -> if (nextStep != Direction.U) return null
+                        '.' -> {
+                            val availableDirections = Direction.values()
+                                .filter {
+                                    val p = (currentLocation + it.vector)
+
+                                    p !in path && p in bounds && map[p] != '#'
+                                }
+
+                            when (availableDirections.size) {
+                                0 -> return null
+                                1 -> nextStep = availableDirections.single()
+                                else -> break
+                            }
+                        }
+
+                        '#' -> return null
+                        else -> throw IllegalStateException("Traversing path $path, looking at $currentLocation")
+                    }
+                }
+
+                return PathSegment(startingPoint, end = currentLocation, length = path.size - 1)
+            }
+
+            val queue = LinkedList<PathSegmentStart>().apply { add(PathSegmentStart(startLocation, Direction.D)) }
+            while (queue.isNotEmpty()) {
+                val startingPoint = queue.poll()
+
+                if (any { it.start == startingPoint }) continue
+
+                val segment = traversePathSegment(startingPoint)
+                if (segment != null) {
+                    add(segment)
+
+                    if (segment.end != finishLocation) {
+                        Direction.values().mapTo(queue) { PathSegmentStart(segment.end, it) }
+                    }
+                }
+            }
+        }
+
+        val possiblePaths = buildList<Path> {
+            val queue = LinkedList<Path>().apply { add(Path(startLocation, emptySet())) }
+            while (queue.isNotEmpty()) {
+                val currentPath = queue.poll()
+
+                segments
+                    .filter { it.start.location == currentPath.currentLocation }
+                    .map { Path(currentLocation = it.end, traversedSegments = currentPath.traversedSegments + it) }
+                    .forEach {
+                        if (it.currentLocation == finishLocation) {
+                            add(it)
+                        } else {
+                            queue.add(it)
+                        }
+                    }
+            }
+        }
+
+        return possiblePaths.maxOf { path -> path.traversedSegments.sumOf { it.length } }
+    }
+}
+
+private data class Path(val currentLocation: Point, val traversedSegments: Set<PathSegment>)
+private data class PathSegmentStart(val location: Point, val direction: Direction)
+private data class PathSegment(val start: PathSegmentStart, val end: Point, val length: Int)
