ec88c10ad8c644a548ad4fb46a01d0ecb5a1dd51
Jerzy Chalupski <jerzy@getbase.com>
2023-12-25 23:57:20 +0100

    2023 Day 24, part 1

--- src/main/java/org/chalup/advent2023/Day24.kt
+++ src/main/java/org/chalup/advent2023/Day24.kt
@@ -1,12 +1,12 @@
 package org.chalup.advent2023
 
-import org.chalup.utils.Vector3
-import kotlin.math.sign
+import java.math.BigDecimal
 
 object Day24 {
     fun task1(input: List<String>, testAreaRange: LongRange): Int {
         val hailstones = input.map { line ->
-            fun parseVector3(text: String) = text.split(", ").map { it.trim().toLong() }.let { (x, y, z) -> Vector3(x, y, z) }
+            fun parseVector3(text: String) =
+                text.split(", ").map { it.trim().toBigDecimal() }.let { (x, y, z) -> Vector3(x, y, z) }
 
             Hailstone(
                 p = line.substringBefore(" @ ").let(::parseVector3),
@@ -21,48 +21,45 @@ object Day24 {
         val intersections = pairs.mapNotNull { (a, b) ->
             val denominator = (a.v.x) * (b.v.y) - (a.v.y) * (b.v.x)
 
-            if (denominator == 0L) {
-                // TODO: this might be a source of errors. Imagine hailstones on coinciding lines
-                //       moving at different speeds. They will collide eventually, and they may
-                //       just collide within the test area bounds.
+            if (denominator == BigDecimal.ZERO) {
                 null
             } else {
-                val nomX = (a.p.x * (a.p.y + a.v.y) - a.p.y * (a.p.x + a.v.x)) * (-b.v.x) - (-a.v.x) * (b.p.x * (b.p.y + b.v.y) - b.p.y * (b.p.x + b.v.x))
-                val nomY = (a.p.x * (a.p.y + a.v.y) - a.p.y * (a.p.x + a.v.x)) * (-b.v.y) - (-a.v.y) * (b.p.x * (b.p.y + b.v.y) - b.p.y * (b.p.x + b.v.x))
+                val c1 = (a.p.x * a.v.y - a.p.y * a.v.x)
+                val c2 = (b.p.x * b.v.y - b.p.y * b.v.x)
+
+                val nomX = c1 * (-b.v.x) - (-a.v.x) * c2
+                val nomY = c1 * (-b.v.y) - (-a.v.y) * c2
 
                 Triple(
                     a, b,
-                    PointF(
-                        nomX.toDouble() / denominator.toDouble(),
-                        nomY.toDouble() / denominator.toDouble(),
+                    Vector3(
+                        nomX / denominator,
+                        nomY / denominator,
+                        z = BigDecimal.ZERO
                     )
                 )
             }
         }
 
-        // I'm getting "too low" hint for 11907. It doesn't make any sense – if anything, I think I'm not
-        // filtering nearly enough collisions. For example, I don't verify the time of collision.
-        // Maybe it's some double math shenanigans?
-        // Also, I'm finding it interesting that we have 11k collisions with just 300 hailstones...
-
-        val testArea = testAreaRange.first.toDouble()..<(testAreaRange.last + 1).toDouble()
+        val testArea = testAreaRange.first.toBigDecimal()..<(testAreaRange.last + 1).toBigDecimal()
 
         return intersections
             .filter { (_, _, p) -> p.x in testArea && p.y in testArea }
-            .filter { (h, _, p) -> sign(p.x - h.p.x) == sign(h.v.x.toDouble()) }
-            .filter { (_, h, p) -> sign(p.x - h.p.x) == sign(h.v.x.toDouble()) }
+            .filter { (h, _, p) -> (p.x - h.p.x).signum() == h.v.x.signum() }
+            .filter { (_, h, p) -> (p.x - h.p.x).signum() == h.v.x.signum() }
             .count()
     }
 
     fun task2(input: List<String>): Long = TODO()
 }
 
-data class PointF(
-    val x: Double,
-    val y: Double,
+private data class Vector3(
+    val x: BigDecimal,
+    val y: BigDecimal,
+    val z: BigDecimal
 )
 
-data class Hailstone(
+private data class Hailstone(
     val p: Vector3,
     val v: Vector3,
 )
