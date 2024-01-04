e93ed50b3ab4064908aaebeb1079d43d7046b865
Jerzy Chalupski <jerzy@chess.com>
2024-01-04 21:56:58 +0100

    2020 Day 18, part 2

--- src/main/java/org/chalup/advent2020/Day18.kt
+++ src/main/java/org/chalup/advent2020/Day18.kt
@@ -1,36 +1,85 @@
 package org.chalup.advent2020
 
-import java.util.LinkedList
+import com.google.common.collect.Iterators
+import org.chalup.advent2020.Expression.GroupedExpression
+import org.chalup.advent2020.Expression.Literal
 
 object Day18 {
-    fun task1(input: List<String>): Long = input
-        .map { evaluate(it.replace(" ", "").iterator()) }
+    fun task1(input: List<String>): Long = calculateSum(input, mapOf('+' to 1, '*' to 1))
+    fun task2(input: List<String>): Long = calculateSum(input, mapOf('+' to 2, '*' to 1))
+
+    private fun calculateSum(input: List<String>, operatorPrecedenceTable: Map<Char, Int>): Long = input
+        .asSequence()
+        .map { it.replace(" ", "") }
+        .map { Parser(it, operatorPrecedenceTable) }
+        .map { it.parseExpression() }
+        .map { evaluate(it) }
         .sum()
 }
 
-private fun evaluate(iterator: CharIterator): Long {
-    fun nextValue(iterator: CharIterator): Long {
-        check(iterator.hasNext())
-        val c = iterator.next()
-
-        return when {
-            c == '(' -> evaluate(iterator)
-            c.isDigit() -> c.digitToInt().toLong()
-            else -> throw IllegalStateException("Next value: $c")
+private fun evaluate(expression: Expression): Long =
+    when (expression) {
+        is GroupedExpression -> evaluate(expression.e)
+        is Expression.InfixExpression -> {
+            val l = evaluate(expression.lhs)
+            val r = evaluate(expression.rhs)
+            when (val operator = expression.operator) {
+                '+' -> l + r
+                '*' -> l * r
+                else -> throw IllegalStateException("Unsupported operator $operator")
+            }
         }
+
+        is Literal -> expression.value.toLong()
     }
 
-    val stack = LinkedList<Long>()
-    stack.push(nextValue(iterator))
+class Parser(
+    input: String,
+    private val precedenceLookup: Map<Char, Int>
+) {
+    private val iterator = Iterators.peekingIterator(input.iterator())
 
-    while (iterator.hasNext()) {
-        when (val c = iterator.next()) {
-            ')' -> return stack.single()
-            '+' -> stack.push(stack.pop() + nextValue(iterator))
-            '*' -> stack.push(stack.pop() * nextValue(iterator))
-            else -> throw IllegalStateException("Evaluate: $c")
+    fun peek(): Char = iterator.peek()
+    fun next(): Char = iterator.next()
+    fun hasNext(): Boolean = iterator.hasNext()
+    fun precedence(char: Char) = precedenceLookup[char] ?: 0
+}
+
+private fun Parser.parseInfixExpression(lhs: Expression): Expression.InfixExpression {
+    val operator = next()
+    val rhs = parseExpression(precedence = precedence(operator))
+    return Expression.InfixExpression(lhs, operator, rhs)
+}
+
+private fun Parser.parsePrefixExpression(): Expression {
+    return when (val c = peek()) {
+        in '0'..'9' -> Literal(next().digitToInt())
+        '(' -> {
+            check(next() == '(')
+            val e = parseExpression(0)
+            check(next() == ')')
+            GroupedExpression(e)
         }
+
+        else -> throw IllegalStateException("Can't parse prefix expression for token $c")
+    }
+}
+
+private fun Parser.parseExpression(precedence: Int = 0): Expression {
+    var left = parsePrefixExpression()
+    while (hasNext() && precedence < precedence(peek())) {
+        left = parseInfixExpression(left)
     }
 
-    return stack.single()
+    return left
+}
+
+private sealed interface Expression {
+    data class Literal(val value: Int) : Expression
+    data class GroupedExpression(val e: Expression) : Expression
+    data class InfixExpression(
+        val lhs: Expression,
+        val operator: Char,
+        val rhs: Expression
+    ) : Expression
 }
