dc9ee5a76508c36d5389db5e84e282bda1417ac9
Jerzy Chalupski <jerzy@chess.com>
2024-07-03 00:00:23 +0200

    2017 Day 7

--- src/main/java/org/chalup/advent2017/Day7.kt
+++ src/main/java/org/chalup/advent2017/Day7.kt
@@ -11,12 +11,40 @@ object Day7 {
                 for (p in programs) removeAll(p.namesOfSupportedPrograms)
             }.single()
         }
+
+    fun task2(input: List<String>): Int {
+        val programs = input.map(::parse)
+        val programsByName = programs.associateBy { it.name }
+
+        fun stackWeight(name: String): Int {
+            val program = programsByName.getValue(name)
+            return program.weight + program.namesOfSupportedPrograms.sumOf { stackWeight(it) }
+        }
+
+        for (program in programs.filter { it.namesOfSupportedPrograms.size > 2 }) {
+            // gotta has at least two other siblings, otherwise it would be ambiguous
+            val groupedWeights = program.namesOfSupportedPrograms.groupBy { stackWeight(it) }
+            if (groupedWeights.size == 2) {
+                val (potentialTargetName, mismatchedGroupWeight) = groupedWeights.mapNotNull { (weight, names) -> names.singleOrNull()?.let { it to weight } }.single()
+                val potentialTarget = programsByName.getValue(potentialTargetName)
+
+                if (potentialTarget.namesOfSupportedPrograms.mapTo(mutableSetOf(), ::stackWeight).size == 1) {
+                    // all children have the same weight, we found our target
+                    val expectedGroupWeight = (groupedWeights.keys - mismatchedGroupWeight).single()
+                    val delta = expectedGroupWeight - mismatchedGroupWeight
+                    return potentialTarget.weight + delta
+                }
+            }
+        }
+
+        throw IllegalArgumentException("Boo")
+    }
 }
 
 private fun parse(text: String): Program = match(text) {
     //language=RegExp
     pattern("""(.*?) \((\d+)\)(?: -> (.*))?""") { (name, weight, subprograms) ->
-        Program(name, weight.toInt(), subprograms.splitToSequence(", ").toSet())
+        Program(name, weight.toInt(), subprograms.split(", ").filter { it.isNotBlank() }.toSet())
     }
 }
 
