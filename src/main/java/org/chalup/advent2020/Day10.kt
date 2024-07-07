dde1d64e233e5ba79225b746bb1a3d6e1b8810ea
Jerzy Chalupski <jerzy@chess.com>
2024-07-07 12:09:00 +0200

    2020 Day 10, part 2

--- src/main/java/org/chalup/advent2020/Day10.kt
+++ src/main/java/org/chalup/advent2020/Day10.kt
@@ -1,8 +1,30 @@
 package org.chalup.advent2020
 
 object Day10 {
-    fun task1(input: List<String>): Int = input
-        .map(String::toInt)
+    fun task1(input: List<String>): Int = adapters(input)
+        .zipWithNext { a, b -> b - a }
+        .groupingBy { it }.eachCount()
+        .let { it.getOrDefault(1, 0) * it.getOrDefault(3, 0) }
+
+    fun task2(input: List<String>): Long = adapters(input)
+        .toSet()
+        .let { adapters ->
+            val cache = mutableMapOf<Long, Long>()
+            fun arrangements(startingAdapter: Long): Long = cache.getOrPut(startingAdapter) {
+                if (startingAdapter == adapters.last()) {
+                    1
+                } else {
+                    adapters
+                        .filter { it - startingAdapter in 1..3 }
+                        .sumOf { arrangements(it) }
+                }
+            }
+
+            arrangements(0L)
+        }
+
+    private fun adapters(input: List<String>) = input
+        .map(String::toLong)
         .sorted()
         .let { adapters ->
             sequence {
@@ -11,7 +33,4 @@ object Day10 {
                 yield(adapters.last() + 3)
             }
         }
-        .zipWithNext { a, b -> b - a }
-        .groupingBy { it }.eachCount()
-        .let { it.getOrDefault(1, 0) * it.getOrDefault(3, 0) }
 }
