88727a6a2b43e103c8a892ed98fef3309e9e851c
Jerzy Chalupski <jerzy@chess.com>
2025-12-07 11:20:07 +0100

    2025 Day 7, part 1

---
+++ src/main/java/org/chalup/advent2025/Day7.kt
@@ -0,0 +1,39 @@
+package org.chalup.advent2025
+
+import org.chalup.utils.Direction
+import org.chalup.utils.Point
+import org.chalup.utils.plus
+import java.util.LinkedList
+
+object Day7 {
+    fun task1(input: List<String>): Int {
+        operator fun List<String>.get(p: Point) = this.getOrNull(p.y)?.getOrNull(p.x)
+
+        val startingPoint = Point(x = input.first().indexOf('S'), y = 0)
+
+        val visitedSplitters = mutableSetOf<Point>()
+        val queue = LinkedList<Point>().apply { add(startingPoint) }
+
+        while (queue.isNotEmpty()) {
+            var p = queue.removeFirst()
+
+            if (p in visitedSplitters) continue
+
+            while (true) {
+                p += Direction.D.vector
+                when (input[p]) {
+                    null -> break
+                    '^' -> {
+                        if (visitedSplitters.add(p)) {
+                            queue += p + Direction.L.vector
+                            queue += p + Direction.R.vector
+                        }
+                        break
+                    }
+                }
+            }
+        }
+
+        return visitedSplitters.size
+    }
+}
